eee6d77d2cd958c51451dd8b74faa5faa42de59a
CarlosChen <carlos.chen@oocl.com>
2019-07-30 09:29:42 +0800

    Fix github login problem about get access token fail.

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -17,10 +17,10 @@ public class AuthorizeController {
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClientId("70a34622407c41973e42");
-        accessTokenDTO.setClientSecret("1de8778ea080c3ff7f885c7498d930b728bcd451");
+        accessTokenDTO.setClient_id("70a34622407c41973e42");
+        accessTokenDTO.setClient_secret("1de8778ea080c3ff7f885c7498d930b728bcd451");
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirectUri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
         accessTokenDTO.setState(state);
         String accessToken = provider.getAccessToken(accessTokenDTO);
         System.out.println(provider.getUser(accessToken).getName());
