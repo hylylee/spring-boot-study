8f8253ffed84f9522751f4c9c71e653ced822bf9
CarlosChen <carlos.chen@oocl.com>
2019-08-01 11:07:19 +0800

    Use application properties to set github setting.

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,15 @@ public class AuthorizeController {
 
     @Autowired
     GithubProvider provider;
+
+    @Value("${github.client.id}")
+    String clientId;
+
+    @Value("${github.client.secret}")
+    String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
