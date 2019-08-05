82c6adcb6c0a746a38e8466a38004145d6fa9389
CarlosChen <carlos.chen@oocl.com>
2019-08-05 23:24:34 +0800

    Save token to db and verify with cookie.

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,16 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                User user = userMapper.findByToken(cookie.getValue());
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.example.demo.mapper;
 import com.example.demo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("INSERT INTO USER (account_id, name, token, gmt_create, gmt_modified) VALUES(#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE TOKEN = #{token}")
+    User findByToken(@Param("token") String token);
 }
