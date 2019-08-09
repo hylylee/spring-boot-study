45978649a521218f7178c7ec5cdd242ef817298a
CarlosChen <carlos.chen@oocl.com>
2019-08-09 21:42:41 +0800

    Support interceptor.

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,52 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.PaginationDTO;
-import com.example.demo.dto.QuestionDTO;
-import com.example.demo.mapper.QuestionMapper;
-import com.example.demo.mapper.UserMapper;
-import com.example.demo.model.Question;
-import com.example.demo.model.User;
 import com.example.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
-    UserMapper userMapper;
-
-    @Autowired
     QuestionService questionService;
 
     @GetMapping("/")
-    public String hello(HttpServletRequest request,
-                        Model model,
+    public String hello(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "2") Integer size) {
-
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null || cookies.length == 0) {
-            return "index";
-        }
-        for (Cookie cookie : cookies) {
-            if ("token".equals(cookie.getName())) {
-                User user = userMapper.findByToken(cookie.getValue());
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-            }
-        }
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -10,15 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-
-    @Autowired
-    UserMapper userMapper;
 
     @Autowired
     QuestionService questionService;
@@ -30,20 +25,7 @@ public class ProfileController {
             @RequestParam(name = "page", defaultValue = "1") Integer page,
             @RequestParam(name = "size", defaultValue = "2") Integer size,
             Model model) {
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null || cookies.length == 0) {
-            return "index";
-        }
-        for (Cookie cookie : cookies) {
-            if ("token".equals(cookie.getName())) {
-                user = userMapper.findByToken(cookie.getValue());
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
